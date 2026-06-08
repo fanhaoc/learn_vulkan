390e3025fc987087268a1105bae709b9b16aa8f8
fanhaoc <1928882425@qq.com>
2026-06-08 17:01:44 +0800

    feat: temp

--- include/data.cpp
+++ include/data.cpp
@@ -33,3 +33,10 @@ const std::vector<Vertex> vertices = {
 const std::vector<uint16_t> indices = {
 	0, 1, 2, 2, 3, 0
 };
+
+// uniforms数据
+struct UniformBufferObject {
+	glm::mat4 model;
+	glm::mat4 view;
+	glm::mat4 proj;
+};
